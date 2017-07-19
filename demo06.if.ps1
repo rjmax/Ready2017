88a30b342b30db7afcea4963a45685ccc1a1658c
Ryan Jones (AZURE RESOURCE MANAGER) <ryjones@microsoft.com>
2017-07-19 13:17:13 -0700

    null fix

--- demo06.if.ps1
+++ demo06.if.ps1
@@ -1,3 +1,3 @@
-﻿New-AzureRmResourceGroup -Name if -Location westus -Force
+﻿New-AzureRmResourceGroup -Name if -Location eastus2euap -Force
 
-New-AzureRmResourceGroupDeployment -ResourceGroupName if -TemplateUri https://raw.githubusercontent.com/rjmax/Ready2017/master/demo06.if.json
+New-AzureRmResourceGroupDeployment -ResourceGroupName if -TemplateUri https://raw.githubusercontent.com/rjmax/Ready2017/master/demo06.if.json 
