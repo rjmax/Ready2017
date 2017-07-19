9ee7ff7095d1c4e5824e8d5394b78080ea912c56
Ryan Jones (AZURE RESOURCE MANAGER) <ryjones@microsoft.com>
2017-07-19 15:29:15 -0700

    if yes and no

--- demo06.if.ps1
+++ demo06.if.ps1
@@ -1,3 +1,7 @@
-﻿New-AzureRmResourceGroup -Name if -Location eastus2euap -Force
+﻿New-AzureRmResourceGroup -Name ifyes -Location eastus2euap -Force
 
-New-AzureRmResourceGroupDeployment -ResourceGroupName if -TemplateUri https://raw.githubusercontent.com/rjmax/Ready2017/master/demo06.if.json 
+New-AzureRmResourceGroupDeployment -ResourceGroupName ifyes -TemplateUri https://raw.githubusercontent.com/rjmax/Ready2017/master/demo06.if.json 
+
+New-AzureRmResourceGroup -Name ifno -Location eastus2euap -Force
+
+New-AzureRmResourceGroupDeployment -ResourceGroupName ifno -TemplateUri https://raw.githubusercontent.com/rjmax/Ready2017/master/demo06.if.json 
