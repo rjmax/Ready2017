c0cbb7701dc284f1da5b893fd5d6d7106f87d486
Ryan Jones (AZURE RESOURCE MANAGER) <ryjones@microsoft.com>
2017-07-19 00:52:37 -0700

    msi cleanup

--- demo07.msiLinuxToArm.ps1
+++ demo07.msiLinuxToArm.ps1
@@ -1,3 +1,8 @@
 ﻿New-AzureRmResourceGroup -Name msiLinux -Location westus -Force
 
 $deployment = New-AzureRmResourceGroupDeployment -ResourceGroupName msiLinux -TemplateUri https://raw.githubusercontent.com/rjmax/Ready2017/master/demo07.msiLinuxToArm.json
+
+$deployment
+
+$deployment.Outputs.commandToGetAToken.Value
+
