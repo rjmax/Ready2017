4efd910fc331782f31d1f2ed63843e0d5c97a163
Ryan Jones (AZURE RESOURCE MANAGER) <ryjones@microsoft.com>
2017-07-20 13:42:06 -0700

    udpating cleanup scripts

--- demo99.cleanup.ps1
+++ demo99.cleanup.ps1
@@ -1,12 +1,12 @@
 ﻿Remove-AzureRmResourceGroup -Name copyDisks -Force
 Remove-AzureRmResourceGroup -Name copySecurityRules -Force
 Remove-AzureRmResourceGroup -Name serialCopies -Force
-Remove-AzureRmResourceGroup -Name crossResourceGroupDeployment -Force
+Remove-AzureRmResourceGroup -Name ryjonesmymgmtrg -Force
+Remove-AzureRmResourceGroup -Name ryjonesmyworkloadrg -Force
 Remove-AzureRmResourceGroup -Name conditionalResourcesSsh -Force
 Remove-AzureRmResourceGroup -Name conditionalResourcesPassword -Force
 Remove-AzureRmResourceGroup -Name ifyes -Force
 Remove-AzureRmResourceGroup -Name ifno -Force
 Remove-AzureRmResourceGroup -Name msiLinux -Force
 Remove-AzureRmResourceGroup -Name msiWindows -Force
-Remove-AzureRmResourceGroup -Name ryjonesmymgmtrg -Force
-Remove-AzureRmResourceGroup -Name ryjonesmyworkloadrg -Force
+
